4ad53488949528957391813c1018565d0c601728
Manzur <rfarfa965@gmail.com>
2025-11-06 20:15:28 -0600

    Se implementó la configuración de doxyfile y cmake

---
+++ build/CMakeFiles/prt7_decoder.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/manzur/GitProjects/politecnica/ds-unidad-2-actividad-2-listasenlazadas-mauricio-manzur/src/ListaDeCarga.cpp" "CMakeFiles/prt7_decoder.dir/src/ListaDeCarga.cpp.o" "gcc" "CMakeFiles/prt7_decoder.dir/src/ListaDeCarga.cpp.o.d"
+  "/home/manzur/GitProjects/politecnica/ds-unidad-2-actividad-2-listasenlazadas-mauricio-manzur/src/RotorDeMapeo.cpp" "CMakeFiles/prt7_decoder.dir/src/RotorDeMapeo.cpp.o" "gcc" "CMakeFiles/prt7_decoder.dir/src/RotorDeMapeo.cpp.o.d"
+  "/home/manzur/GitProjects/politecnica/ds-unidad-2-actividad-2-listasenlazadas-mauricio-manzur/src/SerialReader.cpp" "CMakeFiles/prt7_decoder.dir/src/SerialReader.cpp.o" "gcc" "CMakeFiles/prt7_decoder.dir/src/SerialReader.cpp.o.d"
+  "/home/manzur/GitProjects/politecnica/ds-unidad-2-actividad-2-listasenlazadas-mauricio-manzur/src/TramaLoad.cpp" "CMakeFiles/prt7_decoder.dir/src/TramaLoad.cpp.o" "gcc" "CMakeFiles/prt7_decoder.dir/src/TramaLoad.cpp.o.d"
+  "/home/manzur/GitProjects/politecnica/ds-unidad-2-actividad-2-listasenlazadas-mauricio-manzur/src/TramaMap.cpp" "CMakeFiles/prt7_decoder.dir/src/TramaMap.cpp.o" "gcc" "CMakeFiles/prt7_decoder.dir/src/TramaMap.cpp.o.d"
+  "/home/manzur/GitProjects/politecnica/ds-unidad-2-actividad-2-listasenlazadas-mauricio-manzur/src/main.cpp" "CMakeFiles/prt7_decoder.dir/src/main.cpp.o" "gcc" "CMakeFiles/prt7_decoder.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/prt7_decoder.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prt7_decoder.dir/src/ListaDeCarga.cpp.o"
+  "CMakeFiles/prt7_decoder.dir/src/ListaDeCarga.cpp.o.d"
+  "CMakeFiles/prt7_decoder.dir/src/RotorDeMapeo.cpp.o"
+  "CMakeFiles/prt7_decoder.dir/src/RotorDeMapeo.cpp.o.d"
+  "CMakeFiles/prt7_decoder.dir/src/SerialReader.cpp.o"
+  "CMakeFiles/prt7_decoder.dir/src/SerialReader.cpp.o.d"
+  "CMakeFiles/prt7_decoder.dir/src/TramaLoad.cpp.o"
+  "CMakeFiles/prt7_decoder.dir/src/TramaLoad.cpp.o.d"
+  "CMakeFiles/prt7_decoder.dir/src/TramaMap.cpp.o"
+  "CMakeFiles/prt7_decoder.dir/src/TramaMap.cpp.o.d"
+  "CMakeFiles/prt7_decoder.dir/src/main.cpp.o"
+  "CMakeFiles/prt7_decoder.dir/src/main.cpp.o.d"
+  "prt7_decoder"
+  "prt7_decoder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prt7_decoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
